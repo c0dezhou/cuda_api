160632c40879dbcf9eea2eb0ad48876001d00880
c0dezhou <xiao_y_zhou@163.com>
2023-05-28 23:20:23 +0800

    add bwtest

--- src/driver_api/stress_test/resource_limitation.cpp
+++ src/driver_api/stress_test/resource_limitation.cpp
@@ -1,240 +1,105 @@
 // Resource Limitation
 // 旨在测试cuda driver api的资源限制，例如内存分配，线程数，流数，事件数等。我使用了一些随机的参数和循环
 
-// A stress test for cuda driver api memory allocation and deallocation
-#include <stdio.h>
 #include <cuda.h>
+#include <gtest/gtest.h>
 
-#define N 1000 // Number of memory allocations
-#define M 1024 // Size of each memory allocation in bytes
+// A macro to check CUDA driver API functions
+#define checkError(result) \
+{ \
+    if (result != CUDA_SUCCESS) { \
+        fprintf(stderr, "CUDA driver API error %d at %s:%d\n", result, __FILE__, __LINE__); \
+        FAIL(); \
+    } \
+}
 
-int main() {
-  CUdevice device;
-  CUcontext context;
-  CUresult result;
-  CUdeviceptr devPtr[N]; // Array of device pointers
+// A stress test for cuda driver api memory allocation and deallocation
+TEST(CudaDriverApiTest, MemoryAllocation) {
+    CUdevice device;
+    CUcontext context;
+    CUdeviceptr devPtr[N]; // Array of device pointers
 
-  // Initialize the device and the context
-  result = cuInit(0);
-  if (result != CUDA_SUCCESS) {
-    printf("cuInit failed: %d\n", result);
-    return -1;
-  }
-  result = cuDeviceGet(&device, 0);
-  if (result != CUDA_SUCCESS) {
-    printf("cuDeviceGet failed: %d\n", result);
-    return -1;
-  }
-  result = cuCtxCreate(&context, 0, device);
-  if (result != CUDA_SUCCESS) {
-    printf("cuCtxCreate failed: %d\n", result);
-    return -1;
-  }
+    // Initialize the device and the context
+    checkError(cuInit(0));
+    checkError(cuDeviceGet(&device, 0));
+    checkError(cuCtxCreate(&context, 0, device));
 
-  // Allocate and free memory repeatedly
-  for (int i = 0; i < N; i++) {
-    result = cuMemAlloc(&devPtr[i], M); // Allocate M bytes of device memory
-    if (result != CUDA_SUCCESS) {
-      printf("cuMemAlloc failed: %d\n", result);
-      break;
+    // Allocate and free memory repeatedly
+    for (int i = 0; i < N; i++) {
+        checkError(cuMemAlloc(&devPtr[i], M)); // Allocate M bytes of device memory
+        checkError(cuMemFree(devPtr[i])); // Free the device memory
     }
-    result = cuMemFree(devPtr[i]); // Free the device memory
-    if (result != CUDA_SUCCESS) {
-      printf("cuMemFree failed: %d\n", result);
-      break;
-    }
-  }
 
-  // Destroy the context
-  result = cuCtxDestroy(context);
-  if (result != CUDA_SUCCESS) {
-    printf("cuCtxDestroy failed: %d\n", result);
-    return -1;
-  }
-
-  return 0;
+    // Destroy the context
+    checkError(cuCtxDestroy(context));
 }
 
 // A stress test for cuda driver api thread creation and synchronization
-#include <stdio.h>
-#include <cuda.h>
+TEST(CudaDriverApiTest, ThreadCreation) {
+    CUdevice device;
+    CUcontext context;
+    CUmodule module;
+    CUfunction function;
 
-#define N 1000 // Number of threads per block
-#define M 1000 // Number of blocks per grid
-#define K 1000 // Number of kernel launches
+    // Initialize the device, the context, the module and the function
+    checkError(cuInit(0));
+    checkError(cuDeviceGet(&device, 0));
+    checkError(cuCtxCreate(&context, 0, device));
+    checkError(cuModuleLoad(&module, "dummy_kernel.ptx")); // Load the module from a ptx file
+    checkError(cuModuleGetFunction(&function, module, "dummy_kernel")); // Get the function from the module
 
-__global__ void dummy_kernel() {
-  // Do nothing
-}
-
-int main() {
-  CUdevice device;
-  CUcontext context;
-  CUmodule module;
-  CUfunction function;
-  CUresult result;
-
-  // Initialize the device, the context, the module and the function
-  result = cuInit(0);
-  if (result != CUDA_SUCCESS) {
-    printf("cuInit failed: %d\n", result);
-    return -1;
-  }
-  result = cuDeviceGet(&device, 0);
-  if (result != CUDA_SUCCESS) {
-    printf("cuDeviceGet failed: %d\n", result);
-    return -1;
-  }
-  result = cuCtxCreate(&context, 0, device);
-  if (result != CUDA_SUCCESS) {
-    printf("cuCtxCreate failed: %d\n", result);
-    return -1;
-  }
-  result = cuModuleLoad(&module, "dummy_kernel.ptx"); // Load the module from a ptx file
-  if (result != CUDA_SUCCESS) {
-    printf("cuModuleLoad failed: %d\n", result);
-    return -1;
-  }
-  result = cuModuleGetFunction(&function, module, "dummy_kernel"); // Get the function from the module
-  if (result != CUDA_SUCCESS) {
-    printf("cuModuleGetFunction failed: %d\n", result);
-    return -1;
-  }
-
-  // Launch the kernel repeatedly with different grid and block sizes
-  for (int i = 0; i < K; i++) {
-    int gridDimX = (i % M) + 1; // Vary the grid size from 1 to M
-    int blockDimX = (i % N) + 1; // Vary the block size from 1 to N
-    void *args[] = {}; // No arguments for the kernel
-    result = cuLaunchKernel(function, gridDimX, 1, 1, blockDimX, 1, 1, 0, NULL, args, NULL); // Launch the kernel
-    if (result != CUDA_SUCCESS) {
-      printf("cuLaunchKernel failed: %d\n", result);
-      break;
+    // Launch the kernel repeatedly with different grid and block sizes
+    for (int i = 0; i < K; i++) {
+        int gridDimX = (i % M) + 1; // Vary the grid size from 1 to M
+        int blockDimX = (i % N) + 1; // Vary the block size from 1 to N
+        void *args[] = {}; // No arguments for the kernel
+        checkError(cuLaunchKernel(function, gridDimX, 1, 1, blockDimX, 1, 1, 0, NULL, args, NULL)); // Launch the kernel
+        checkError(cuCtxSynchronize()); // Synchronize the context
     }
-    result = cuCtxSynchronize(); // Synchronize the context
-    if (result != CUDA_SUCCESS) {
-      printf("cuCtxSynchronize failed: %d\n", result);
-      break;
-    }
-  }
 
-  // Unload the module and destroy the context
-  result = cuModuleUnload(module);
-  if (result != CUDA_SUCCESS) {
-    printf("cuModuleUnload failed: %d\n", result);
-    return -1;
-  }
-  result = cuCtxDestroy(context);
-  if (result != CUDA_SUCCESS) {
-    printf("cuCtxDestroy failed: %d\n", result);
-    return -1;
-  }
-
-  return 0;
+    // Unload the module and destroy the context
+    checkError(cuModuleUnload(module));
+    checkError(cuCtxDestroy(context));
 }
 
 // A stress test for cuda driver api stream creation and destruction
-#include <stdio.h>
-#include <cuda.h>
+TEST(CudaDriverApiTest, StreamCreation) {
+    CUdevice device;
+    CUcontext context;
+    CUstream stream[N]; // Array of streams
 
-#define N 1000 // Number of streams to create and destroy
+    // Initialize the device and the context
+    checkError(cuInit(0));
+    checkError(cuDeviceGet(&device, 0));
+    checkError(cuCtxCreate(&context, 0, device));
 
-int main() {
-  CUdevice device;
-  CUcontext context;
-  CUresult result;
-  CUstream stream[N]; // Array of streams
-
-  // Initialize the device and the context
-  result = cuInit(0);
-  if (result != CUDA_SUCCESS) {
-    printf("cuInit failed: %d\n", result);
-    return -1;
-  }
-  result = cuDeviceGet(&device, 0);
-  if (result != CUDA_SUCCESS) {
-    printf("cuDeviceGet failed: %d\n", result);
-    return -1;
-  }
-  result = cuCtxCreate(&context, 0, device);
-  if (result != CUDA_SUCCESS) {
-    printf("cuCtxCreate failed: %d\n", result);
-    return -1;
-  }
-
-  // Create and destroy streams repeatedly
-  for (int i = 0; i < N; i++) {
-    result = cuStreamCreate(&stream[i], CU_STREAM_DEFAULT); // Create a stream with default flags
-    if (result != CUDA_SUCCESS) {
-      printf("cuStreamCreate failed: %d\n", result);
-      break;
+    // Create and destroy streams repeatedly
+    for (int i = 0; i < N; i++) {
+        checkError(cuStreamCreate(&stream[i], CU_STREAM_DEFAULT)); // Create a stream with default flags
+        checkError(cuStreamDestroy(stream[i])); // Destroy the stream
     }
-    result = cuStreamDestroy(stream[i]); // Destroy the stream
-    if (result != CUDA_SUCCESS) {
-      printf("cuStreamDestroy failed: %d\n", result);
-      break;
-    }
-  }
 
-  // Destroy the context
-  result = cuCtxDestroy(context);
-  if (result != CUDA_SUCCESS) {
-    printf("cuCtxDestroy failed: %d\n", result);
-    return -1;
-  }
-
-  return 0;
+    // Destroy the context
+    checkError(cuCtxDestroy(context));
 }
 
 // A stress test for cuda driver api event creation and destruction
-#include <stdio.h>
-#include <cuda.h>
+TEST(CudaDriverApiTest, EventCreation) {
+    CUdevice device;
+    CUcontext context;
+    CUevent event[N]; // Array of events
 
-#define N 1000 // Number of events to create and destroy
+    // Initialize the device and the context
+    checkError(cuInit(0));
+    checkError(cuDeviceGet(&device, 0));
+    checkError(cuCtxCreate(&context, 0, device));
 
-int main() {
-  CUdevice device;
-  CUcontext context;
-  CUresult result;
-  CUevent event[N]; // Array of events
-
-  // Initialize the device and the context
-  result = cuInit(0);
-  if (result != CUDA_SUCCESS) {
-    printf("cuInit failed: %d\n", result);
-    return -1;
-  }
-  result = cuDeviceGet(&device, 0);
-  if (result != CUDA_SUCCESS) {
-    printf("cuDeviceGet failed: %d\n", result);
-    return -1;
-  }
-  result = cuCtxCreate(&context, 0, device);
-  if (result != CUDA_SUCCESS) {
-    printf("cuCtxCreate failed: %d\n", result);
-    return -1;
-  }
-
-  // Create and destroy events repeatedly
-  for (int i = 0; i < N; i++) {
-    result = cuEventCreate(&event[i], CU_EVENT_DEFAULT); // Create an event with default flags
-    if (result != CUDA_SUCCESS) {
-      printf("cuEventCreate failed: %d\n", result);
-      break;
+    // Create and destroy events repeatedly
+    for (int i = 0; i < N; i++) {
+        checkError(cuEventCreate(&event[i], CU_EVENT_DEFAULT)); // Create an event with default flags
+        checkError(cuEventDestroy(event[i])); // Destroy the event
     }
-    result = cuEventDestroy(event[i]); // Destroy the event
-    if (result != CUDA_SUCCESS) {
-      printf("cuEventDestroy failed: %d\n", result);
-      break;
-    }
-  }
 
-  // Destroy the context
-  result = cuCtxDestroy(context);
-  if (result != CUDA_SUCCESS) {
-    printf("cuCtxDestroy failed: %d\n", result);
-    return -1;
-  }
-
-  return 0;
+    // Destroy the context
+    checkError(cuCtxDestroy(context));
 }
